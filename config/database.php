99ba499c0ca5e8416b65ec1ceae7041e3cc2fe05
Luke Risser <lukerisser04@gmail.com>
2025-09-05 19:31:43 -0400

    changed db connection to jawsdb

--- config/database.php
+++ config/database.php
@@ -45,12 +45,27 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            // Parse JAWSDB_URL if present
+            ...((function () {
+                $url = env('JAWSDB_URL');
+                if ($url) {
+                    $parts = parse_url($url);
+                    return [
+                        'host' => $parts['host'],
+                        'port' => $parts['port'] ?? '3306',
+                        'database' => ltrim($parts['path'], '/'),
+                        'username' => $parts['user'],
+                        'password' => $parts['pass'],
+                    ];
+                }
+                return [
+                    'host' => env('DB_HOST', '127.0.0.1'),
+                    'port' => env('DB_PORT', '3306'),
+                    'database' => env('DB_DATABASE', 'laravel'),
+                    'username' => env('DB_USERNAME', 'root'),
+                    'password' => env('DB_PASSWORD', ''),
+                ];
+            })()),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
