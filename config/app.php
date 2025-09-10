5f4feb3c859581aad1d9b31d9756cfb101fe0858
mneff12 <mn1253@messiah.edu>
2025-09-10 13:27:44 -0400

    Update APP_URL for Heroku deployment

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://falconteams-2bdbf6dec657.herokuapp.com'),
 
     /*
     |--------------------------------------------------------------------------
