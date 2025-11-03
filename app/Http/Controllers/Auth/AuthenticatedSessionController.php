86f9f086c68a4d7f411fd69025f65962379f4138
mneff12 <mn1253@messiah.edu>
2025-11-03 16:18:22 -0500

    Update AuthenticatedSessionController.php

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -46,7 +46,7 @@ class AuthenticatedSessionController extends Controller
             return redirect()->intended('/coach-dashboard');
         }
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        return redirect()->intended(route('home', absolute: false));
     }
 
     public function destroy(Request $request): RedirectResponse
