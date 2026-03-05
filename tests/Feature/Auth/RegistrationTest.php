3c80a43957cb841cbbe9c7ffa67571f36197c506
MukisaSS <mserufusa@gmail.com>
2026-03-04 19:28:05 -0500

    Removed and updated some test files

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Tests\Feature\Auth;
+use App\Models\Camp;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
@@ -13,14 +14,17 @@ class RegistrationTest extends TestCase {
     }
 
     public function test_new_users_can_register(): void {
+        $response = $this->get('/register');
+
         $response = $this->post('/register', [
-            'name' => 'Test User',
+            'fname' => 'Test',
+            'lname' => 'User',
             'email' => 'test@example.com',
             'password' => 'password',
             'password_confirmation' => 'password',
         ]);
 
-        $this->assertAuthenticated();
-        $response->assertRedirect(route('dashboard', absolute: false));
+        $response->assertStatus(302);
+        $response->assertRedirect('/verify-email');
     }
 }
